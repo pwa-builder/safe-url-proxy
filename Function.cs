bdf567ef6f95bae68ebbd5177bf300b71d46079d
Judah Gabriel Himango <judahgabriel@gmail.com>
2021-06-02 15:42:37 -0700

    Returning error code from fetched URL

--- Function.cs
+++ Function.cs
@@ -32,13 +32,19 @@ namespace PWABuilder.SafeImage
                 Version = new Version(2, 0)
             };
             var getResult = await http.SendAsync(getMsg);
-            getResult.EnsureSuccessStatusCode();
+
+            //getResult.EnsureSuccessStatusCode();
+            if (!getResult.IsSuccessStatusCode)
+            {
+                return new StatusCodeResult((int)getResult.StatusCode);
+            }
+            
             var contentType = getResult.Content.Headers.ContentType;
             var contentLength = getResult.Content.Headers.ContentLength;
             if (contentLength > 20000000)
             {
                 log.LogError("Content length must be 20MB or less");
-                return new BadRequestResult();
+                return new UnsupportedMediaTypeResult();
             }
 
             var imgStream = await getResult.Content.ReadAsStreamAsync();
